81c640bac8197e24a6e285a8695a0095fd929a9f
FantomLis <creativegamerprofile@gmail.com>
2024-11-10 15:23:07 +0300

    Trying to update original mod to .Net Standart 2.1

--- UnityTemplate/Assets/Scripts/Assembly-CSharp/IHaveUIData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-// Token: 0x02000087 RID: 135
-public interface IHaveUIData
-{
-	// Token: 0x0600022B RID: 555
-	string GetString();
-}
--- UnityTemplate/Assets/Scripts/Assembly-CSharp/ItemInstance.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-// Token: 0x020001ED RID: 493
-public class ItemInstance : MonoBehaviour
-{
-
-}
