0608ecbbd091a5fbd0d8e949396484d5e9fb47cc
FantomLis <creativegamerprofile@gmail.com>
2024-12-13 22:03:16 +0300

    Finally fixed errors with tooltips (very badly)

--- Patches/ItemInstanceDataPatch.cs
+++ Patches/ItemInstanceDataPatch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using HarmonyLib;
+using UnityEngine;
 
 namespace FantomLis.BoomboxExtended.Patches;
 [HarmonyPatch(typeof(ItemInstanceData))]
@@ -8,18 +9,19 @@ public class ItemInstanceDataPatch
 
     [HarmonyFinalizer]
     [HarmonyPatch(nameof(ItemInstanceData.GetEntryIdentifier))]
-    static Exception GetEntryIdentifier(System.Type type,ref byte __result)
+    static Exception GetEntryIdentifier(Exception __exception,System.Type type,ref byte __result)
     {
         if (type == typeof (MusicEntry))
             __result = 11;
         else if (type == typeof (VolumeEntry))
             __result = 12;
+        Debug.LogError(__exception);
         return null;
     }
     
     [HarmonyFinalizer]
     [HarmonyPatch(nameof(ItemInstanceData.GetEntryType))]
-    public static Exception GetEntryType(byte identifier, ref ItemDataEntry __result)
+    public static Exception GetEntryType(Exception __exception,byte identifier, ref ItemDataEntry __result)
     {
         switch (identifier)
         {
@@ -28,7 +30,7 @@ public class ItemInstanceDataPatch
             case 12:
                 __result = (ItemDataEntry) new VolumeEntry();break;
         }
-
+        Debug.LogError(__exception);
         return null;
     }
 }
---
+++ Patches/TooltipPatch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+
+namespace FantomLis.BoomboxExtended.Patches;
+
+[HarmonyPatch(typeof(Item))]
+public class TooltipPatch
+{
+    [HarmonyFinalizer]
+    [HarmonyPatch(nameof(Item.GetLocalizedDisplayName))]
+    public static void GetLocalizedDisplayName(Item __instance, ref string __result)
+    {
+        if (__instance.name.ToLower().Trim().Equals("Boombox".ToLower()))
+        {
+            __result = __instance.name;
+        }
+    }
+
+    [HarmonyFinalizer]
+    [HarmonyPatch(nameof(Item.GetTootipData))]
+    public static void GetTooltipData(Item __instance, ref IEnumerable<IHaveUIData> __result)
+    {
+        if (__instance.name.ToLower().Trim().Equals("Boombox".ToLower()))
+        {
+            var x = new List<IHaveUIData>();
+            x.Add(new ItemKeyTooltip(__instance.name, new InteractKeybindSetting(), new List<ControllerGlyphs.GlyphType>([ControllerGlyphs.GlyphType.UseItem])));
+            __result = x;
+        }
+        
+        if (__instance.name.ToLower().Trim().Equals("Boombox_ToolTips".ToLower()))
+        {
+            var x = new List<IHaveUIData>();
+            x.Add(new ItemKeyTooltip(__instance.name, new InteractKeybindSetting(), new List<ControllerGlyphs.GlyphType>([ControllerGlyphs.GlyphType.UseItem])));
+            __result = x;
+        }
+        
+    }
+}
