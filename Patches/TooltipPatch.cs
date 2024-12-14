f3d3784bea8811b01872dca68a64f28f9b3398ac
FantomLis <creativegamerprofile@gmail.com>
2024-12-14 14:20:16 +0300

    Now Tooltips use real localization

--- Patches/TooltipPatch.cs
+++ Patches/TooltipPatch.cs
@@ -9,31 +9,30 @@ public class TooltipPatch
 {
     [HarmonyFinalizer]
     [HarmonyPatch(nameof(Item.GetLocalizedDisplayName))]
-    public static void GetLocalizedDisplayName(Exception __exception,Item __instance, ref string __result)
+    public static void GetLocalizedDisplayName(Exception __exception, Item __instance, ref string __result)
     {
         if (__instance.name.ToLower().Trim().Equals(Boombox.ItemName.ToLower()))
         {
-            __result = __instance.name;
+            __result = LocalizationStrings.Boombox;
+            return;
         }
+
+        if (__exception != null) throw __exception;
     }
 
     [HarmonyFinalizer]
     [HarmonyPatch(nameof(Item.GetTootipData))]
-    public static void GetTooltipData(Exception __exception,Item __instance, ref IEnumerable<IHaveUIData> __result)
+    public static void GetTooltipData(Exception __exception, Item __instance, ref IEnumerable<IHaveUIData> __result)
     {
         if (__instance.name.ToLower().Trim().Equals(Boombox.ItemName.ToLower()))
         {
             var x = new List<IHaveUIData>();
-            x.Add(new ItemKeyTooltip(__instance.name, new InteractKeybindSetting(), new List<ControllerGlyphs.GlyphType>([ControllerGlyphs.GlyphType.UseItem])));
+            x.Add(new ItemKeyTooltip(LocalizationStrings.Boombox_ToolTip1, new InteractKeybindSetting(), new List<ControllerGlyphs.GlyphType>([ControllerGlyphs.GlyphType.UseItem])));
+            x.Add(new ItemKeyTooltip(LocalizationStrings.Boombox_ToolTip2, new ToggleSelfieModeKeybindSetting(), new List<ControllerGlyphs.GlyphType>([ControllerGlyphs.GlyphType.SecondaryUseItem])));
             __result = x;
+            return;
         }
-        
-        if (__instance.name.ToLower().Trim().Equals("Boombox_ToolTips".ToLower()))
-        {
-            var x = new List<IHaveUIData>();
-            x.Add(new ItemKeyTooltip(__instance.name, new InteractKeybindSetting(), new List<ControllerGlyphs.GlyphType>([ControllerGlyphs.GlyphType.UseItem])));
-            __result = x;
-        }
-        
+
+        if (__exception != null) throw __exception;
     }
 }
