2fd08b07934b46d56af732be7c31176ccdf3b8d3
FantomLis <creativegamerprofile@gmail.com>
2024-12-29 15:26:46 +0300

    Moving BaseEntry listing from methods

--- Patches/ItemInstanceDataPatch.cs
+++ Patches/ItemInstanceDataPatch.cs
@@ -4,19 +4,21 @@ using System.Linq;
 using System.Reflection;
 using FantomLis.BoomboxExtended.Entries;
 using HarmonyLib;
+using UnityEngine;
 
 namespace FantomLis.BoomboxExtended.Patches;
 [HarmonyPatch(typeof(ItemInstanceData))]
 public class ItemInstanceDataPatch
 {
+    static List<Type> BaseEntries = AppDomain.CurrentDomain.GetAssemblies()
+        .SelectMany(assembly => assembly.GetTypes())
+        .Where(type => type.IsSubclassOf(typeof(BaseEntry))).ToList();
+    static List<BaseEntry> BaseEntriesInstance = BaseEntries.Select(x => (BaseEntry) Activator.CreateInstance(x)).ToList();
 
     [HarmonyFinalizer]
     [HarmonyPatch(nameof(ItemInstanceData.GetEntryIdentifier))]
     static Exception GetEntryIdentifier(Exception __exception,System.Type type,ref byte __result)
     {
-        List<Type> BaseEntries = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(assembly => assembly.GetTypes())
-            .Where(type => type.IsSubclassOf(typeof(BaseEntry))).ToList();
         if (BaseEntries.Contains(type)) __result = ((BaseEntry)Activator.CreateInstance(type)).ID();
         else if (__exception != null) throw __exception;
         return null;
@@ -26,11 +28,9 @@ public class ItemInstanceDataPatch
     [HarmonyPatch(nameof(ItemInstanceData.GetEntryType))]
     public static Exception GetEntryType(Exception __exception,byte identifier, ref ItemDataEntry __result)
     {
-        List<BaseEntry> BaseEntriesInstance = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(assembly => assembly.GetTypes())
-            .Where(type => type.IsSubclassOf(typeof(BaseEntry))).Select(x => (BaseEntry) Activator.CreateInstance(x)).ToList();
+        BaseEntriesInstance.ForEach(x => Debug.Log(x.GetType().Name));
         if (BaseEntriesInstance.Exists(x => x.ID() == identifier))
-            __result = BaseEntriesInstance.Find(x => x.ID() == identifier);
+            __result = (ItemDataEntry) Activator.CreateInstance(BaseEntriesInstance.Find(x => x.ID() == identifier).GetType());
         else if (__exception != null) throw __exception;
         return null;
     }
